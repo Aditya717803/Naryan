e9e78ef52a13e916e7a4d8e2bf2fea379bb4addc
Adityakumar Sahu <133566895+Aditya717803@users.noreply.github.com>
2024-09-01 07:45:22 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the path and file extensions as needed
+    './public/index.html', // Add paths to your HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
